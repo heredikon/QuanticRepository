a70d41ba0fe10fd1f7a9645fdb7bfad945142580
heredikon <heredia800@gmail.com>
2019-09-09 04:58:21 -0500

    advancve

--- src/main/java/com/mycompany/cuanticrepository/Basic.java
+++ src/main/java/com/mycompany/cuanticrepository/Basic.java
@@ -19,26 +19,26 @@ public class Basic {
     public Complex sum(Complex x, Complex y) {
         ans.setReal(x.getReal() + y.getReal());
         ans.setImaginary(x.getImaginary() + y.getImaginary());
-        return new Complex(ans.getReal(),ans.getImaginary());
+        return new Complex(ans.getReal(), ans.getImaginary());
     }
 
     public Complex subs(Complex x, Complex y) {
         ans.setReal(x.getReal() - y.getReal());
         ans.setImaginary(x.getImaginary() - y.getImaginary());
-        return new Complex(ans.getReal(),ans.getImaginary());
+        return new Complex(ans.getReal(), ans.getImaginary());
     }
 
     public Complex mult(Complex x, Complex y) {
         ans.setReal(x.getReal() * y.getReal() - x.getImaginary() * y.getImaginary());
         ans.setImaginary(x.getImaginary() * y.getReal() + x.getReal() * y.getImaginary());
-        return new Complex(ans.getReal(),ans.getImaginary());
+        return new Complex(ans.getReal(), ans.getImaginary());
     }
 
     public Complex divide(Complex x, Complex y) {
         double denominator = Math.pow(y.getReal(), 2) + Math.pow(y.getImaginary(), 2);
         ans.setReal(((y.getReal() * y.getReal()) + (x.getImaginary() * y.getImaginary())) / denominator);
         ans.setImaginary(((y.getReal() * x.getImaginary()) - (x.getReal() * y.getImaginary())) / denominator);
-        return new Complex(ans.getReal(),ans.getImaginary());
+        return new Complex(ans.getReal(), ans.getImaginary());
 
     }
 
@@ -91,7 +91,7 @@ public class Basic {
         for (int i = 0; i < v1.length; i++) {
             ans = sum(ans, mult(V[i], v2[i]));
         }
-        return new Complex(ans.getReal(),ans.getImaginary());
+        return new Complex(ans.getReal(), ans.getImaginary());
     }
 
     public boolean equalsV(Complex[] v1, Complex[] v2) {
@@ -101,6 +101,16 @@ public class Basic {
             }
         }
         return true;
+    }
+    
+    public Complex[][] identityM(int length){
+        Complex[][] ansM = new Complex[length][length];
+        for (int i = 0;i<length;i++){
+            for (int j = 0;j<length;j++){
+                ansM[i][j] = i == j ?new Complex(1,0) : new Complex(0,0);
+            }
+        }
+        return ansM;
     }
 
     public boolean equalsM(Complex[][] m1, Complex[][] m2) {
@@ -137,19 +147,75 @@ public class Basic {
         }
         return ansM;
     }
-    
-    public Complex[][] conjugadoM(Complex[][] m1){
+
+    public Complex[][] conjugadoM(Complex[][] m1) {
         Complex[][] ansM = new Complex[m1.length][m1[0].length];
-        for (int i=0;i<ansM.length;i++){
+        for (int i = 0; i < ansM.length; i++) {
             ansM[i] = conjugadoV(m1[i]);
         }
         return ansM;
     }
-    public Complex[][] attachedM(Complex[][] m1){
+
+    public Complex[][] attachedM(Complex[][] m1) {
         Complex[][] ansM = new Complex[m1.length][m1[0].length];
         ansM = transpM(conjugadoM(m1));
         return ansM;
     }
+
+    public Complex[][] multM(Complex[][] m1, Complex[][] m2) {
+        Complex[][] ansM = new Complex[m1.length][m2[0].length];
+        Complex result;
+        int i;
+        int j;
+        for (int i_f = 0; i_f < m1.length; i_f++) {
+            for (int j_f = 0; j_f < m2[0].length; j_f++) {
+                result = new Complex(0, 0);
+                i = 0;
+                j = 0;
+                while (j < m1[0].length && i < m2.length) {
+                    result = sum(result, mult(m1[i_f][j], m2[i][j_f]));
+                    j++;
+                    i++;
+                }
+                ansM[i_f][j_f] = result;
+            }
+        }
+        return ansM;
+    }
+
+    public Complex trace(Complex[][] m1) {
+        Complex ans = new Complex(0, 0);
+        for (int i = 0; i < m1.length && i < m1[0].length; i++) {
+            ans = sum(ans, m1[i][i]);
+        }
+        return ans;
+    }
+
+    public Complex internalprodM(Complex[][] m1, Complex[][] m2) {
+        return trace(multM(m1, m2));
+    }
+
+    public Complex[] accion(Complex[][] m1, Complex[] v1) {
+        Complex[] ansV = new Complex[v1.length];
+        for (int i = 0; i < m1.length; i++) {
+            ansV[i] = new Complex(0,0);
+            for (int j = 0; j < m1.length; j++) {
+                ansV[i] = sum(ansV[i], mult(m1[i][j],v1[j]));
+            }
+        }
+        return ansV;
+    }
+    
+    public boolean isHermitian(Complex[][] m1){
+        return equalsM(m1, attachedM(m1));
+    }
+    
+    public boolean isUnitary(Complex[][] m1){
+        return equalsM(multM(m1, attachedM(m1)), identityM(m1.length));
+    }
+    
+    
+    
 
     public Complex[][] tensorProduct(Complex[][] m1, Complex[][] m2) {
         Complex[][] ansM = new Complex[m1.length * m2.length][m1[0].length * m2[0].length];
@@ -159,12 +225,13 @@ public class Basic {
                 for (int p = 0; p < m2.length; p++) {
                     for (int q = 0; q < m2[0].length; q++) {
                         System.out.println(i + " , " + j + " , " + p + " , " + q);
-                        ansM[i * m2.length + p][j * m2[0].length + q] = mult(m1[i][j], m2[p][q]);               
+                        ansM[i * m2.length + p][j * m2[0].length + q] = mult(m1[i][j], m2[p][q]);
                     }
                 }
             }
         }
         return ansM;
     }
+    public Complex distance()
 
 }
